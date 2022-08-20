50a1fece04ebf94a3ec88181cb6bc92c8aa3d916
Ashutosh Singh <you@example.com>
2022-08-20 22:58:31 +0530

    made header responsive

--- src/organisms/header/Header.tsx
+++ src/organisms/header/Header.tsx
@@ -2,10 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Popover } from "react-tiny-popover";
 import "./Header.scss";
 import {
+  faBars,
   faCartShopping,
   faSearch,
   faSignOut,
   faUser,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { useAuth } from "../../hooks/useAuth";
 import Button from "../../atoms/button/Button";
@@ -18,16 +20,18 @@ interface IHeader {
 const Header: React.FC<React.PropsWithChildren<IHeader>> = ({
   className = "",
 }) => {
+
+  const [menuBtnClicked, setMenuBtnClicked] = useState(false);
   const { logout, currentUser } = useAuth();
 
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
   return (
-    <nav className="Header">
+    <nav className={`Header ${menuBtnClicked && "new-header"} `}>
       <div>
         <img className="header-logo" alt="logo" src="/img/logo.png" />
       </div>
-      <ul className="header-category">
+      <ul className={` ${menuBtnClicked && "new-header-category"} header-category`}>
         <li>
           <Button variation="secondary">Men</Button>
         </li>
@@ -39,7 +43,7 @@ const Header: React.FC<React.PropsWithChildren<IHeader>> = ({
         </li>
       </ul>
 
-      <div className="header-icons">
+      <div className={` header-icons`}>
         <Button variation="secondary">
           <FontAwesomeIcon icon={faSearch} />
         </Button>
@@ -74,6 +78,13 @@ const Header: React.FC<React.PropsWithChildren<IHeader>> = ({
             </Button>
           </Popover>
         )}
+      </div>
+      <div
+        className="menu-btn"
+        onClick={() => setMenuBtnClicked(!menuBtnClicked)}
+      >
+        {(!menuBtnClicked && <FontAwesomeIcon icon={faBars} />) ||
+          (menuBtnClicked && <FontAwesomeIcon icon={faXmark} fontSize={30} />)}
       </div>
     </nav>
   );
